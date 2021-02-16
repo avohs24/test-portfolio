3af8e6bfc8426e786d7a9c935c140813fcf4dbcd
avohs24 <alex@wirewheel.io>
2021-02-15 20:23:26 -0500

    wip.

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-var backgrounds = ["#11cbd7", "#c6f1e7", "#f0fff3", "#fa4659"]; 
-var textColors = ["#fa4659", "#f0fff3", "#c6f1e7", "#11cbd7"];
 var colorCombos = [
   {
-    background: "#d8e2dc",
-    text: "#9d8189"
+    background: "#004e64",
+    text: "#FFFFFF"
   },
   {
-    background: "#ffe5d9",
-    text: "#f4acb7"
+    background: "#00a5cf",
+    text: "#FFFFFF"
   },
   {
-    background: "#ffcad4",
-    text: "#d8e2dc"
+    background: "#9fffcb",
+    text: "#000000"
   },
   {
-    background: "#f4acb7",
-    text: "#ffe5d9"
+    background: "#25a18e",
+    text: "#FFFFFF"
   },
   {
-    background: "#9d8189",
-    text: "#d8e2dc"
+    background: "#7ae582",
+    text: "#000000"
   },
 ];
-function setColor(el) { 
+function setColor(el, event) { 
+  event.preventDefault();
   const name = document.getElementById('typedName');
+  const buttonTxt1 = document.getElementById('button-txt-1');
+  const buttonTxt2 = document.getElementById('button-txt-2');
   const tagline = document.getElementById('tagline');
   const icons = document.querySelectorAll('.fab')
   el.colorIdx = el.colorIdx || 0; 
@@ -31,6 +32,8 @@ function setColor(el) {
   el.style.backgroundColor = colorCombos[idx % colorCombos.length].background;
   name.style.color = colorCombos[idx % colorCombos.length].text;
   tagline.style.color = colorCombos[idx % colorCombos.length].text;
+  buttonTxt1.style.color = colorCombos[idx % colorCombos.length].text;
+  buttonTxt2.style.color = colorCombos[idx % colorCombos.length].text;
   icons.forEach((icon) => {
     icon.style.fill = colorCombos[idx % colorCombos.length].text;
     icon.style.color = colorCombos[idx % colorCombos.length].text;
